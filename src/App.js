b4ff8b39ff0c41f010aab4477503a6b1a499e911
reactpractice <darahmed55@gmail.com>
2023-05-10 23:10:57 +0500

    Movie WebSites Using Json Data and Props

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
 // import logo from './logo.svg';
 import './App.css';
-import Student from './components/Student';
-
-//import Header,{Footer} from './Header';
-// import {Header, Footer} from './Header';
-// import Header from './Header';
-// import Application from './Header';
+import { Header } from './components/Header';
+import { Movie } from './components/Movie';
+import  movies from './movies.json';
 
 function App() {
-  //let name = "Web3Mantra"
   return (
-    <div className="App">
-      <Student 
-        name="Ahmed Raza"
-        age={29} 
-      />
-
-      <Student 
-        name="Asgar Raza"
-        age={31} 
-      />
-
-      <Student 
-        name="Akbar Raza"
-        age={32} 
-      />
-
+    <div className="App">  
+      <Header />
+      <div className="main">
+      {
+          movies.map((element, index) => {
+            return (
+              <Movie 
+                    key={index}
+                    title={element.Title} 
+                    year={element.Year}
+                    img={element.Poster}
+              />
+             )
+          })
+      }          
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Header.css'
+
+export const Header = () => {
+  return (
+    <div className="header">
+        <h1 id="Headerh1">Movie Review</h1>
+    </div>
+  )
+}
